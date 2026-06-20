a5c92c07d0adfa2c931466b0fad80009d3da83a9
THETQV <tanishqtqv@gmail.com>
2026-06-20 17:06:43 +0530

    y

--- f-sensor/sensor.cpp
+++ f-sensor/sensor.cpp
@@ -2,44 +2,55 @@
 #include <MPU6050.h>
 
 
+
     
 MPU6050 mpu;
-
 float gxo_off = 0, gyo_off = 0, gzo_off = 0;
     float axo_off = 0, ayo_off = 0, azo_off = 0;
 void setup() {
+    Serial.begin(115200);
+    delay(10);
 
-    //variables
+      
+      
+
     
-
-    int samples = 8000;
-   Wire.begin();
-   Wire.setClock(400000); 
+   Wire.begin(); 
+   delay(5);
    mpu.initialize();
+   int samples = 2000;
    int i = 0;
+    int sumAXO = 0, sumAYO = 0, sumAZO = 0;
+   int sumGXO = 0, sumGYO = 0, sumGZO = 0;
 
-   while(i < 20000000) {
+   while(i < samples) {
+    
     //for acceleration
-    int16_t ax_raw, ay_raw, az_raw, sumAXO = 0, sumAYO = 0, sumAZO = 0;
+    int16_t ax_raw, ay_raw, az_raw;
     mpu.getAcceleration(&ax_raw, &ay_raw, &az_raw);
     sumAXO += ax_raw;
     sumAYO += ay_raw;
     sumAZO += (az_raw - 16384);
     //for gyroscope
-    int16_t gx_raw, gy_raw, gz_raw, sumGXO = 0, sumGYO = 0, sumGZO = 0;
+    delay(1);
+    int16_t gx_raw, gy_raw, gz_raw;
     mpu.getRotation(&gx_raw, &gy_raw, &gz_raw);
     sumGXO += gx_raw;
     sumGYO += gy_raw;
     sumGZO += gz_raw;   
     //get offsets
-    axo_off = sumAXO / samples;
+    
+    i++;
+    delay(5);
+
+    
+}
+axo_off = sumAXO / samples;
     ayo_off = sumAYO / samples;
     azo_off = sumAZO / samples;
     gxo_off = sumGXO / samples;
     gyo_off = sumGYO / samples;
     gzo_off = sumGZO / samples;
-    i = micros();
-}
 
 }
 void loop(){
@@ -64,7 +75,7 @@ float gz_e = gz - gzo_off;
     float gyroRollRate  = gy_e / 131.0f;
     float gyroYawRate   = gz_e / 131.0f;
 
-    Serial.begin(250000);
+  
     Serial.print("Estimated Pitch: ");
     Serial.print(ePitch);
     Serial.print(" | Estimated Roll: ");
